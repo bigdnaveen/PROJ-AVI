0c12c6b6da0b7eacfbfd19241ac009d97128bee2
bigdnaveen <102846128+bigdnaveen@users.noreply.github.com>
2023-03-20 09:52:32 +0530

    Create wordreplace.py
    replac the word  from which you want

---
+++ wordreplace.py
@@ -0,0 +1,8 @@
+def replace_word():
+     
+    str="hi i am naveen ,will you be friend "
+    word_to_replace = input ("enter the word to replace: ")
+    word_to_replacement=input("enter the word to get replace:")
+    print(str.replace(word_to_replace,word_to_replacement))
+
+replace_word()    
